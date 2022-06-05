bff62f07af5f2e10a426015a336ac4c1fc46dd24
Scott <Scott@DESKTOP-L5UD7TV>
2022-06-05 01:15:11 -0500

    expertise

--- src/sections/expertise/code/code.component.jsx
+++ src/sections/expertise/code/code.component.jsx
@@ -4,7 +4,23 @@ import './code.component.scss';
 const Code = () => {
     return(
         <section className='code'>
-            Data structures, Design Patterns, and Performant measurable code. 
+             <div className='left-container'>
+                <img src="./img/code.png" alt="code"></img>
+                <div className="subheader">building industry standard products</div>
+                <p>
+                    I use the lastest technology to write readable, performant, testable, <span>code</span>. 
+                    I specialize in the front-end, but I have experience in every facet of the dev lifecycle.
+                    
+                    Data structures, Design Patterns, and Performant measurable code. 
+                </p>
+                <p>
+                    Primary: HTML5, SCSS | LESS, Angular.js, Angular2+, React16+, C#, .Net Core, Git
+                </p>
+                <p>Secondary: Mongo DB, Java, Azure DevOps</p>
+            </div>
+            <div className='right-container'>
+                <img src="./img/gears.png" alt="gears"></img>
+           </div>
         </section>
     )
 }
--- src/sections/expertise/ui/ui.component.jsx
+++ src/sections/expertise/ui/ui.component.jsx
@@ -4,7 +4,21 @@ import './ui.component.scss';
 const UI = () => {
     return(
         <section className='ui'>
-            <img src="./img/ui.png" alt="ux"></img>
+           <div className='left-container'>
+                <img src="./img/ui.png" alt="ux"></img>
+                <div className="subheader">creating visual products to build trust in the brand</div>
+                <p>
+                    Once the UX designs are approved, I then begin implementing the look and feel of the <span>user interface</span>. 
+                    This includes colors and imagery that creates feeling and trust in the end user.
+                </p>
+                <p>
+                    I achieve this by: High fidility compositions, Spec sheets, logos and graphics, creating style sheets, design library
+                </p>
+                <p>Adobe Illustrator, Adobe Photoshop, Adobe Premiere, Adobe Affter Effects</p>
+            </div>
+            <div className='right-container'>
+                <img src="./img/brain.png" alt="brain"></img>
+           </div>
         </section>
     )
 }
